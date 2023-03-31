0fa4d9ed07b297d97d87a237e6347b20a5f271c7
wawa_yang <2393147374@qq.com>
2023-03-31 18:06:06 +0800

    loss_successful

---
+++ utils/loss.py
@@ -0,0 +1,42 @@
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# CrossEntropyLoss其实就是Softmax–Log–NLLLoss合并成一步
+class CrossEntropyLoss2d(nn.Module):
+
+    def __init__(self, weight=None):
+        super(CrossEntropyLoss2d,self).__init__()
+
+        self.loss = nn.NLLLoss(weight)
+
+    def forward(self, outputs, targets):
+        return self.loss(F.log_softmax(outputs, dim=1), targets)  
+
+loss_func = nn.CrossEntropyLoss()
+
+input = torch.randn((1, 3, 5, 5), requires_grad=True)
+print(input)
+target = torch.tensor([[[0,1,2,2,2],
+                       [0,1,2,1,2],
+                       [0,1,1,2,2],
+                       [0,1,2,1,2],
+                       [0,1,2,1,2]
+                       ]])
+print(target)
+print(target.shape)
+output = loss_func(input, target)
+print(output)
+
+loss1 = CrossEntropyLoss2d()
+output1 = loss1(input, target)
+print(output1)
+'''
+ 输入给模型的数据维度应该是 4 维的,(batch_size, C, H, W),语义分割标签维度应该是 3 维的:(batch_size, H, W)
+模型预测输出的通道数应该等于语义分割的类别数；
+语义分割标签图像的像素值应该在 [0, 类别数-1] 范围内。若不在，读进来之后必须进行转换。   cityscape 有 19类  ,标签为0-18  
+
+'''
+
